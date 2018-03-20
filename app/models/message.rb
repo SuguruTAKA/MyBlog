a26e5ddf2dd91230e571e8b686b7f07532d02c43
Suguru Takahashi <bo9iro@gmail.com>
2018-03-20 05:28:35 +0000

    New column

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
     validates :title,length:{maximum:32},presence: true
     validates :message,length:{maximum:256},presence: true
     validates :name,length:{maximum:32}
-    validates :rating,numericality:{minimum:0,maximum:10}
+    validates :rating,numericality:{less_than_or_equal_to:10}
 end
